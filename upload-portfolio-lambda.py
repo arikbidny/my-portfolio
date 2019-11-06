c98e97dd279b9ce7df11b632a1b9e237e0dc35ad
Arik Bidny <arikbidny@gmail.com>
2019-11-06 15:13:16 +0200

    Adding lambda fixes

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,5 +1,4 @@
 import json
-
 import boto3
 import zipfile
 import StringIO
@@ -11,13 +10,26 @@ def lambda_handler(event, context):
     topic = sns.Topic(
         'arn:aws:sns:eu-central-1:705556567441:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuildbucket.arikbidny.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact['location']['s3Location']
+        print("Building portfolio from " + str(location))
+
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('portfolio.arikbidny.com')
-        build_bucket = s3.Bucket('portfoliobuildbucket.arikbidny.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -26,9 +38,12 @@ def lambda_handler(event, context):
                                                 ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-        print 'Job Done'
+        print('Job Done')
         topic.publish(Subject="Portfolio Deployed",
                       Message="Portfolio Deployed Successfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Portfolio Deployed Failed",
                       Message="Portfolio Deployed Failed!")
